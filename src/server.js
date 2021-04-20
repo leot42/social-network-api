6dbe564dfb792c85987eca44722c223df70ee1a1
Lionel Townsend <townsendlionel@gmail.com>
2021-04-20 12:43:52 -0500

    route setup

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// const models = require('./models');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialNetworkDb', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('debug', true);
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
